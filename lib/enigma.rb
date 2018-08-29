06d6562987ee7ec2a5c5df32a53dff8a11eadac0
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-29 17:56:31 -0600

    doesnt all work

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,6 @@
 require 'date'
 require "./lib/key"
+require 'pry'
 
 class Enigma
 
@@ -7,13 +8,14 @@ class Enigma
               :date,
               :message
 
-  def initialize
+  # def initialize
+  #   @key = Key.new
+  # end
 
-  end
-
-  def encrypt(message, key = Key.new.random, date = Date.today.strftime("%e%m%y"))
-    @key = key
+  def encrypt(message, key = Key.new.random_key, date = Date.today.strftime("%d%m%y"))
     @date = date
     @message = message
+    # @key = Key.new(key)
   end
+
 end
--- lib/key.rb
+++ lib/key.rb
@@ -1,7 +1,22 @@
 class Key
-attr_reader :random
- def initialize
+attr_reader :random_key,
+            :key_string
+
+def initialize(key = random_key)
+  @key_string = key
+end
+
+ def random_key
   @random = rand(00000..99999).to_s.rjust(5, '0')
  end
+
+ def key_holder
+   held_keys = []
+   held_keys << @key_string.chars.join[0..1].to_i
+   held_keys << @key_string.chars.join[1..2].to_i
+   held_keys << @key_string.chars.join[2..3].to_i
+   held_keys << @key_string.chars.join[3..4].to_i
+ end
+
 
 end
