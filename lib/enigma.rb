a90b3ae5dd6b1316295d4c045627fcaf74f1e455
Averi Johnson <averimj@live.com>
2018-08-29 18:55:12 -0600

    wip

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -16,4 +16,19 @@ class Enigma
     @date = date
     @message = message
   end
+
+  def slice_it
+    require "pry"; binding.pry
+    @message.each_slice(4) do |rotation_group|
+      p rotation_group
+    end
+    # ["h", "e", "l", "l"], ["o"]
+  end
+
+
 end
+
+
+# I think offset and key should be in the same test. Even though their doing their own thing now, eventually they have to work together. like adding offset and key.
+
+# when we slice_it..."h" = message[0]. We want to some how match-up "a" rotation to index [0] too. (YEAH BUT HOW GET IT TO DO THIS...I know)
